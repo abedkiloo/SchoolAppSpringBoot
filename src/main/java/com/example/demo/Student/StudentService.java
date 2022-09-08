ff4571cc3d3e8809c5d5f480a818a864ba254f35
abedkiloo <abedxh@gmail.com>
2022-09-08 20:39:02 +0300

    first commit

---
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.Student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Service
+public class StudentService {
+    public  List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Abednego Wambua",
+                        "abedxh@gmail.com",
+                        LocalDate.of(2995, Month.JANUARY, 13),
+                        27
+
+                ));
+    }
+}
